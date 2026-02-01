161b3e150850a7ecd5d4cf54cb474023f6ce3e5c
Melquisedeque <melquisedequevital@gmail.com>
2026-02-01 01:04:35 -0300

    criação da pasta do backend

---
+++ sigt-backend/src/main/java/br/edu/ifpb/sigt_backend/SigtBackendApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.sigt_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigtBackendApplication.class, args);
+	}
+
+}
---
+++ sigt-backend/src/test/java/br/edu/ifpb/sigt_backend/SigtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.sigt_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
